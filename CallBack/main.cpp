c2367d1496d6ef08205ad5db6a1266a3e5d505c3
José Rossini <joserossini57@gmail.com>
2019-03-06 16:50:29 +0000

    Update main.cpp

--- CallBack/main.cpp
+++ CallBack/main.cpp
@@ -1,60 +1,12 @@
 #include <functional>
 
-class CallBack
-{
-	public:
-		CallBack() {};
-		void addCall(std::function<void(int*)> call){
-			_call = call;
-		}
-		void executeCall(int * i) {
-			_call(i);
-		}
-	private:
-		std::function<void(int*)> _call;
+template <typename T, typename... Args>
+class CallBack {
+        public:
+            CallBack(std::function<T(Args...)> func) : _func(func) {};
+            void execute(Args... args) const { _func(std::forward<Args>(args)...); };
+
+        private:
+            std::function<T(Args...)> _func;
 };
-
-class CallBackCenter
-{
-	public:
-		CallBackCenter(){};
-		void addCallBack(CallBack *callBack){
-			_callBack = callBack;
-		};
-		void run(){
-			int * i = new int(10);
-			_callBack->executeCall(i);
-		};
-		
-	private:
-		CallBack *_callBack;
-};
-
-class MyClass
-{
-	public:
-		MyClass(CallBackCenter  & center):_callBack(new CallBack) {
-			_callBack->addCall(std::bind(&MyClass::printInt,this,std::placeholders::_1));
-			center.addCallBack(_callBack);
-		};
-		
-		void printInt(int * i) {
-			printf("%d",*i);
-			}
-		
-	private:
-		CallBack * _callBack;
-};
-
-int main ()
-{
-	
-		CallBackCenter call = CallBackCenter();
-		MyClass myclass = MyClass(call);
-	
-		call.run();
-	
-	return 0;
-}
-
 
